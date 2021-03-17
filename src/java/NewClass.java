4755efa9b02d657c9a208266c46f6b56ff0e173d
MinhT <phammanhtoanhht933@gmail.com>
2021-03-17 09:58:07 +0700

    1st-to GitHub

---
+++ src/java/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MinhT
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+        System.out.println("this í 1st uploaf connect to remote GitHub sever");
+    }
+}
